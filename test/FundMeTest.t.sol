446eba7b09333a06ed74c6442280b525181e1883
Queisme <queiskey@gmail.com>
2024-10-29 21:17:55 -0500

    update

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,5 +1,16 @@
 //SPDX-License-Identifier: MIT
 
+/*
+
+4 different types of testing:
+
+1. Unit - testing a specific part of our code
+2. Integration - testing how our code works with other parts of our code
+3. Forked - testing our code on a simulated real environment
+4. Staging - testing our code in a real environment that is not production
+
+*/
+
 pragma solidity ^0.8.27;
 
 import {Test, console} from "forge-std/Test.sol";
@@ -23,6 +34,7 @@ contract FundMeTest is Test {
     }
 
     function testPriceFeedVersionIsAccurate() public {
+        
         uint256 version = fundMe.getVersion();
         assertEq(version, 4);
     }
